ddc7d769af1f363b3cdb8bf64b31a0991980f57e
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2017-12-09 02:20:16 -0200

    Initial structure adding entities, a dao example and hibernate configuration

---
+++ src/daos/AbstractDao.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daos;
+
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import utils.HibernateUtil;
+
+/**
+ *
+ * @author leonardo
+ */
+@SuppressWarnings("unchecked")
+public abstract class AbstractDao<T> {
+ 
+    private Class<T> entityClass;
+    protected final SessionFactory sessionFactory = HibernateUtil
+            .getSessionFactory();
+ 
+    public AbstractDao(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+ 
+    public AbstractDao() {
+    }
+ 
+    public List<T> findAll() {
+        try {
+            if (!sessionFactory.getCurrentSession().getTransaction().isActive())
+                sessionFactory.getCurrentSession().getTransaction().begin();            
+            return sessionFactory.getCurrentSession()
+                    .createQuery("from " + entityClass.getName()).list();
+        } catch (RuntimeException re) {
+            return null;
+        }
+    }
+ 
+    public void update(T instance) {
+        try {
+            if (!sessionFactory.getCurrentSession().getTransaction().isActive())
+                sessionFactory.getCurrentSession().getTransaction().begin();            
+            sessionFactory.getCurrentSession().merge(instance);
+            sessionFactory.getCurrentSession().getTransaction().commit();
+        } catch (RuntimeException re) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+            throw re;
+        }
+    }
+ 
+    public void delete(T instance) {
+        try {
+            if (!sessionFactory.getCurrentSession().getTransaction().isActive())
+                sessionFactory.getCurrentSession().getTransaction().begin();            
+            sessionFactory.getCurrentSession().delete(instance);
+            sessionFactory.getCurrentSession().getTransaction().commit();
+        } catch (RuntimeException re) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+            throw re;
+        }
+    }
+ 
+    public void create(T instance) {
+        try {
+            if (!sessionFactory.getCurrentSession().getTransaction().isActive())
+                sessionFactory.getCurrentSession().getTransaction().begin();            
+            sessionFactory.getCurrentSession().persist(instance);
+            sessionFactory.getCurrentSession().getTransaction().commit();
+        } catch (RuntimeException re) {
+            sessionFactory.getCurrentSession().getTransaction().rollback();
+            throw re;
+        }
+    }
+ 
+    public T find(Object primarykey) {
+        try {
+            if (!sessionFactory.getCurrentSession().getTransaction().isActive())
+                sessionFactory.getCurrentSession().getTransaction().begin();            
+            return (T) sessionFactory.getCurrentSession().get(entityClass,
+                    (Serializable) primarykey);
+        } catch (RuntimeException re) {
+            return null;
+        }
+    }
+}
---
+++ src/daos/TireBrandDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daos;
+
+import entities.TireBrand;
+
+/**
+ *
+ * @author leonardo
+ */
+public class TireBrandDao extends AbstractDao<TireBrand>{
+    
+    public TireBrandDao(){
+        super(TireBrand.class);
+    }
+    
+}
